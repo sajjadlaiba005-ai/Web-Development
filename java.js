ba905ff34a44e5b5b56614040d965c69d377d3fe
sajjadlaiba005-ai <sajjadlaiba005@gmail.com>
2026-03-08 10:06:33 +0500

    Task 10 Javascript File

---
+++ java.js
@@ -0,0 +1,91 @@
+console.log('Hello')
+alert('Hello alert!')
+x = 10 -> value
+variable 
+Register Form
+
+username = 'fiza'
+email = 'fiza@gmail.com'
+city = 'faisalabad'
+code = 38000
+
+console.log(40)
+console.log("hello's")
+
+console.log(username, email, city, code)
+console.log(email)
+console.log(city)
+console.log(code)
+
+code
+characters
+String '', ""
+
+
+String, Number -> Data Types
+Variable 
+
+
+keyword var , let, const ,
+    data types (String, number, boolean, null, undefine, use typeof)
+prompt
+
+text => string
+variable -> assign -> value
+$user123$name = ''
+_user123_name = ''
+user123user = 'data';
+console.log(userName)
+var
+var userName;
+userName = 'Fatima'
+console.log(userName)
+
+const
+const username = 'fatima'
+username = 'arshad'
+
+
+
+declare
+
+var rollno;
+
+rollno = 1234
+console.log(rollno)
+
+rollno = 2323
+
+
+console.log(rollno)
+
+prompt
+
+
+let email;
+console.log(email)
+email = prompt("Enter your email:")
+document.getElementById("demo").innerHTML = `
+<h1> ${email}</h1>`;
+
+Text + variable
+const username = 'fatima'
+// console.log('Hello ' , username , 123 , 'Welcome')
+console.log(Hello ${ username })
+
+Operator
+arithmetic(* , + - , %)
+assignment(*= , += , -= , %=)
+camparison(>=,<=)
+
+operands
+
+let first_no = 2
+let second_no = 4
+let result;
+result = first_no + second_no
+result = first_no - second_no
+result = first_no * second_no
+result = first_no / second_no
+result = first_no % second_no
+console.log(result)
